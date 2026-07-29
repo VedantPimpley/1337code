ad72a4b0a30188838bd5f3c4221ecaec4a1ccd10
vedant.pimpley <vedantpimpley7@gmail.com>
2026-07-28 18:26:49 -0700

    Add House Robbers 2.py

---
+++ House Robber 2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        '''
+        requires:
+            len(nums) >= 1
+            all values in nums are >= 0
+        ensures:
+            res is max, aware of circular list
+        algo:
+            n == 0 -> nums[0]
+
+            
+        '''
+
+        n = len(nums)
+        # edge case
+        if n == 1:
+            return nums[0]
+
+        # general case
+        def hr_1(numbers:list[int]) -> int:
+            m = len(numbers)
+            f = {i: None for i in range(m)}
+            f[-1], f[-2] = 0, 0
+            
+            for i in range(m):
+                f[i] = max(f[i-1], f[i-2] + numbers[i])
+
+            return f[m-1]
+
+        res = max(hr_1(nums[1:]), hr_1(nums[:n-1]))
+        return res
